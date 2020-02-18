ef232012532cd7a0d9faec0bb93e15e1ee822088
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2020-02-18 11:34:25 -0500

    Add files via upload

---
+++ Cajero.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Cajero
+    {
+        private string CodigoCajero;
+        private short CantidadX = 0;
+        private short CantidadXX = 0;
+        private short CantidadL = 0;
+        private short CantidadMaxX = 60;
+        private short CantidadMaxXX = 60;
+        private short CantidadMaxL = 60;
+
+        public Cajero()
+        {
+            CantidadX1 = 60;
+            CantidadXX1 = 60;
+            CantidadL1 = 60;
+        }
+
+        public short CantidadX1 { get => CantidadX; set => CantidadX = value; }
+        public short CantidadXX1 { get => CantidadXX; set => CantidadXX = value; }
+        public short CantidadL1 { get => CantidadL; set => CantidadL = value; }
+
+        public bool Leer_Tarjeta()
+        {
+            Random rnd = new Random();
+            int v1 = rnd.Next(0, 2);
+            if (v1 == 0)
+            {
+                return false;
+            }
+            else return true;
+        }
+        public int totalDineroCajilla()
+        {
+            
+        }
+    }
+}
