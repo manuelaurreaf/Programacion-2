a54360dff2f6a9bf3b709ccaade7f82b8def1dc9
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2020-02-24 15:27:53 -0500

    Add files via upload

---
+++ Extracto.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extracto.Clases
+{
+    class Extracto
+    {
+        private string Nombre;
+        private UInt16 NroId;
+        private UInt64 Telefono;
+        private UInt16 SaldoPuntos;
+
+        public string nombre
+        {
+            get { return Nombre.ToUpper(); }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new Exception("Nombre ingresado invalido");
+                }
+
+            }
+        }
+        public ushort nroId
+        {
+            get { return NroId; }
+            set
+            {
+                if (value > 0)
+                {
+                    NroId = value;
+                }
+                else throw new Exception("Numero de ID no es valido");
+
+            }
+        }
+        public ulong telefono
+        {
+            get { return telefono; }
+            set
+            {
+                if (value > 999999 && value <= 9999999999)
+                {
+                    telefono = value;
+                }
+                else throw new Exception("Numero de ID no es valido");
+
+            }
+        }
+        public ushort saldoPuntos { get => SaldoPuntos; set => SaldoPuntos = value; }
+
+        public Extracto(string nombre, ushort nroId, ulong telefono)
+        {
+            try
+            {
+                Nombre = nombre;
+                NroId = nroId;
+                Telefono = telefono;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Porblema al crear el objeto");
+            }
+        }
+        public ulong AcumularPuntos(ulong ValorCompra)
+        {
+            ulong ValorAcumulado = 0;
+            try
+            {
+                if (ValorCompra >= 100) ValorAcumulado = ValorCompra % 100;
+                return ValorAcumulado;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Porblema al crear el objeto");
+            }
+        }
+        public bool RedimirPuntos(ulong Puntos)
+        {
+            //bool Redimir = false;
+            try
+            {
+                if (SaldoPuntos >= Puntos)
+                {
+                    SaldoPuntos -= (ushort)Puntos;
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                throw new Exception("No se puede redimir los puntos");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extracto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PuntosArticulo());
+        }
+
+        public static uint PuntosArticulo()
+        {
+            uint puntos = 0;
+            short Opcion;
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Articulos que puede escoger:");
+                    Console.WriteLine("1. Articulo de decoracion cuesta 5.000 puntos.");
+                    Console.WriteLine("2. Articulo electronico menor cuesta 10.000 puntos.");
+                    Console.WriteLine("3. Articulo electronico mediano cuesta 50.000 puntos.");
+                    Console.WriteLine("4. Tiquete aereo nacional cuesta 90.000 puntos.");
+                    Console.WriteLine("5. Articulo electronico grande cuesta 150.000 puntos.");
+                    Console.WriteLine("6. Tiquete aereo round trip nacional cuesta 180.000 puntos.");
+                    Console.WriteLine("Ingrese que opcion quiere: ");
+
+                    try { Opcion = short.Parse(Console.ReadLine()); }
+                    catch (Exception) { Opcion = 0; }
+                } while (Opcion < 1 && Opcion > 6);
+                return puntos;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Porblema al crear el objeto");
+            }
+        }
+    }
+}
